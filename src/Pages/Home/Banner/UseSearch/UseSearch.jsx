d0c6a881a21a7119aa4d999ef6216defc1373463
tamanna986 <tamanna.986jahan@gmail.com>
2023-11-27 22:28:33 +0600

    implemented react awesome button in banner

--- src/Pages/Home/Banner/UseSearch/UseSearch.jsx
+++ src/Pages/Home/Banner/UseSearch/UseSearch.jsx
@@ -3,7 +3,7 @@
 const UseSearch = () => {
     return (
         
-            <form className="p-5 my-10 w-3/4  mx-auto">
+            <form className="p-5 my-1 lg:my-10 w-3/4  mx-auto">
  <input type="text" placeholder="Search By Tags" className="  px-9 w-full border-none input outline-none" />
             </form>
         
